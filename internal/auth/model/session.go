b8d20d6c4d36b75a8fe831088a6761240bd71878
okaabe <okaabe2006@gmail.com>
2024-08-07 14:17:37 -0300

    feat: add user and session model

---
+++ internal/auth/model/session.go
@@ -0,0 +1,21 @@
+package model
+
+type Session struct {
+    Name string
+    Email string
+
+    IssuedAt int
+    ExpiresAt int
+}
+
+func NewSession(name, email string, issuedAt, expiresAt int) (*Session, error) {
+    session := Session{
+        Name: name,
+        Email: email,
+        IssuedAt: issuedAt,
+        ExpiresAt: expiresAt
+    }
+
+    return &session, nil
+}
+
---
+++ internal/auth/model/user.go
@@ -0,0 +1,24 @@
+package model
+
+type User struct {
+	Id        string
+	Name      string
+	Email     string
+	Password  string
+	CreatedAt int
+	UpdatedAt int
+}
+
+func NewUser(id, name, email, password string, createdAt, updatedAt int) (*User, error) {
+    user := User{
+        Id: id,
+        Name: name,
+        Email: email,
+        Password: password,
+        CreatedAt: createdAt,
+        UpdatedAt: updatedAt,
+    }
+
+    return &user, nil
+}
+
